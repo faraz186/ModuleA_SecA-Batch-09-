b60669fa3e7ce4721a67ec37ad64016839f71f56
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-08-31 23:35:44 +0500

    Add files via upload

---
+++ Class 12 (JS) (31-08-2023)/app.js
@@ -0,0 +1,64 @@
+// ------------------------------------------------------------------
+
+console.log("Welcome JS");
+
+// document.write("Welcome JS");
+
+alert("Welcome JS");
+
+// ------------------------------------------------------------------
+
+var number = 5;
+
+console.log(number);
+
+// ------------------------------------------------------------------
+
+var firstName;
+
+firstName = "Muhammad";
+
+console.log(firstName);
+
+// ------------------------------------------------------------------
+var number;
+
+var result = number + 100;
+
+document.write(result);
+
+// ------------------------------------------------------------------
+
+var status = true;
+
+console.log(status);
+
+// ------------------------------------------------------------------
+
+var status2 = false;
+
+console.log(status2)
+
+// ------------------------------------------------------------------
+
+var firstName = "Muhammad";
+
+var lastName = "Faraz";
+
+console.log(firstName + " " +lastName);
+
+// ------------------------------------------------------------------
+
+var firstName = "Muhammad";
+
+var lastName = "Faraz";
+
+console.log(firstName + " " + lastName);
+
+// ------------------------------------------------------------------
+
+var number1 = 10;
+
+var number2 = 5;
+
+console.log(number1 + number2);
