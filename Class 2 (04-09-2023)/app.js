2e265b5f10692d4940a751d2b7b0ccb8a73ee1ea
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-09-04 21:35:09 +0500

    Add files via upload

---
+++ Class 2 (04-09-2023)/app.js
@@ -0,0 +1,103 @@
+// alert("Welcome to JS");
+
+// Swal.fire("Good job!", "You clicked the button!", "success");
+
+// var number = 10;
+
+// var number = 12;
+
+// console.log(typeof number);
+
+// alert(number);
+
+// var firstPerson = "xyz";
+
+// firstPerson = "abc";
+
+// console.log(firstPerson);
+
+// var num = 12;
+
+// num = num + 20;
+
+// 12 + 20
+
+// console.log(num);
+
+// var a = "faraz";
+
+// console.log(a);
+
+// var firstName = "abc";
+
+// var num1 = 24;
+
+// var num2 = 11;
+
+// var res = num1 + num2;
+
+// console.log(num1 / num2);
+
+// var num = 12;
+
+// var res = --num;
+
+// console.log(res);
+
+// console.log(num);
+
+// var num = 10;
+
+// num--;
+
+// num = num - 1;
+
+// var res = num - 1;
+
+// console.log(num);
+
+// var calc = (10 + 12) * (8 / 4);
+
+// console.log(calc);
+
+// var a = 5;
+// var b = 2;
+
+// var res = a++ + b++ - ++a - --b + ++a - b++ - --a;
+5 + 2 - 7 - 2 + 8 - 2 - 7;
+// console.log(res);
+
+// -1, -2 , -3, -4,
+
+// var num = 10;
+
+// var res = ++num;
+
+// console.log(res);
+
+// var firstName = "Muhammad";
+
+// var lastName = "ismail";
+
+// var fullName = firstName + " " + lastName;
+
+// console.log(fullName);
+
+// var num1 = "2";
+
+// var num2 = 4;
+
+// console.log(num1 - num2);
+
+
+// chapter no 1
+
+// Q1
+
+
+
+// Q2
+
+
+
+// chaptetr 1 ends here
