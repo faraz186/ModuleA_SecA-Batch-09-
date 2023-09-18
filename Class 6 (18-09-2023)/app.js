9886582af1fe6ee99ec732324a013e27a8141696
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-09-18 21:33:25 +0500

    Add files via upload

---
+++ Class 6 (18-09-2023)/app.js
@@ -0,0 +1,78 @@
+// *********************************For Loop****************************************
+
+// for (var i = 0; i <= 10; i++) {
+//   console.log("welcome user", i);
+// }
+
+// var arr = ["faraz", "arsalan", "rabi", "haseeb", "shahid"];
+
+// for (var i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// var userInput = prompt("Where do you live??");
+
+// userInput = userInput.toUpperCase();
+
+// var city = ["KARACHI", "LAHORE", "islamabad", "peshawar"];
+
+// var Status = false;
+
+// for (var i = 0; i < city.length; i++) {
+//   if (userInput === city[i]) {
+//     Status = true;
+//   }
+// }
+
+// if (Status) {
+//   console.log("match");
+// } else {
+//   console.log("not mach");
+// }
+
+// var name = "Muhammad Faraz";
+
+// console.log(name.toLowerCase());
+
+// var userInput = prompt("Enter your name");
+
+// userInput = userInput.toUpperCase();
+
+// var NamesArr = ["Faraz", "Arsalan", "Mustufa", "Haseeb"];
+
+// var flags = "no";
+
+// for (var i = 0; i < NamesArr.length; i++) {
+//   if (userInput === NamesArr[i]) {
+//     flags = "yes";
+//   }
+// }
+
+// if (flags === "yes") {
+//   console.log("match");
+// } else {
+//   console.log("not match");
+// }
+
+// for (var i = 0; i < 5; i++) {
+//   for (var j = 0; j < 3; j++) {
+//     console.log(i, j);
+//   }
+// }
+
+// var arr = ["faraz", [123, false]];
+
+// for (var i = 0; i < arr.length; i++) {
+//   for (var j = 0; j < arr[i].length; j++) console.log(arr[i][j]);
+// }
+
+
+// tasks:   
+
+// make a program take user input to check whether the given string is palindrome or not..
+
+
+// civic
+// madam
+// level
+// refer
