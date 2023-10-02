7b92e50a3361fcbff94b3d3c5ed93356c43b3c70
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-10-02 21:20:10 +0500

    dates task and functions...

---
+++ Class 9 (02-10-2023)/app.js
@@ -0,0 +1,105 @@
+// var date = new Date();
+
+// console.log(date);
+
+// var nextRamdan = new Date("10 march 2024");
+
+// var nextRamdanMilli = nextRamdan.getTime();
+
+// var todayDate = new Date();
+
+// var todayDateMilli = todayDate.getTime();
+
+// console.log(nextRamdanMilli);
+// console.log(todayDateMilli);
+
+// var difference = nextRamdanMilli - todayDateMilli;
+
+// console.log(difference);
+
+// var months = difference / (1000 * 60 * 60 * 24 * 30);
+// console.log(months);
+
+// var days = (months - Math.floor(months)) * 24;
+
+// var hours = (days - Math.floor(days)) * 60;
+
+// var minutes = (hours - Math.floor(hours)) * 60;
+
+// console.log("Months==== " + Math.floor(months));
+
+// console.log("Days==== " + Math.floor(days));
+
+// console.log("Hours==== " + Math.floor(hours));
+
+// console.log("minutes==== " + Math.floor(minutes));
+
+// // var sec;
+// var sec = 1 * 1000;
+// var minutes = sec * 60;
+// var hours = minutes * 60;
+// var day = hours * 24;
+// var month = day * 30;
+// // console.log(month);
+
+// var month = Math.floor(Difference / 259200000);
+// var day = Math.floor(Difference / 259200000);
+
+// ********************Functions************************
+
+function popUp() {
+  alert("Hello World");
+}
+
+// for (var i = 0; i <= 10; i++) {
+//   popUp();
+// }
+
+// function sum(x, y) {
+//   // parameters
+//   console.log(x + y);
+// }
+
+// var num1 = +prompt("Enter first value");
+// var num2 = +prompt("Enter second value");
+
+// sum(num1, num2); //arguments
+
+// function multiplicationTable(num, length) {
+//   for (var i = 1; i <= length; i++) {
+//     document.write(num + "x" + i + "=" + num * i + "<br>");
+//   }
+// }
+
+// var number = +prompt("Enter number to generate table");
+// var len = +prompt("Enter table length");
+
+// multiplicationTable(number, len);
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// var result = sum(10, 12);
+
+// console.log(result);
+
+// function calc(num1, opr, num2) {
+//   if (opr === "+") {
+//     return num1 + num2;
+//   } else if (opr === "-") {
+//     return num1 - num2;
+//   } else if (opr === "*") {
+//     return num1 * num2;
+//   } else if (opr === "/") {
+//     return num1 / num2;
+//   }
+// }
+
+// var num1 = +prompt("Enter first value");
+// var operator = prompt("Enter operator");
+// var num2 = +prompt("Enter second value");
+
+// var result = calc(num1, operator, num2);
+
+// console.log(result);
