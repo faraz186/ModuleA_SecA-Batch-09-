41d7273f2e69376f18017f39c5c6d0d1e14a2952
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-10-09 21:01:37 +0500

    Add files via upload

---
+++ Class 11 (09-10-2023)/app.js
@@ -0,0 +1,65 @@
+// function completePara() {
+//     var text =
+//       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, similique. Quasi reiciendis aspernatur eos esse deleniti voluptate id fuga saepe fugiat dicta illum iusto necessitatibus harum sed, eius nobis! Aliquid, iure, labore optio dicta ut dolorum blanditiis est ullam eos fuga eveniet dignissimos rem sequi! Maxime neque tempora illum quasi?";
+//   var para = document.getElementById("para");
+//   para.innerHTML += "<h1>Hello WORLD</h1>";
+// }
+
+// function originalPara() {
+//   var text1 =
+//     " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos,";
+//   var para = document.getElementById("para");
+//   para.innerHTML = text1;
+// }
+
+// tEMPALTE LITERAL
+
+// var firstName = "Muhammad";
+
+// var lastName = "Faraz";
+
+// console.log(firstName + " " + lastName);
+
+// console.log(`My name is ${firstName} ${lastName}.I am a web developer`);
+
+// function makeInvisible() {
+//   var img = document.getElementById("img");
+//   img.className += " hidden";
+// }
+
+// function changeImage(id, ImageLink) {
+//   var img = document.getElementById(id);
+//   img.src = ImageLink;
+// }
+
+// function changeImageBefore(id, ImageLink) {
+//   var img = document.getElementById(id);
+//   img.src = ImageLink;
+// }
+
+// var anchor = document.getElementById("link");
+
+// anchor.href = "https://www.google.com";
+// anchor.target = "_blank";
+
+// var para = document.getElementById("para");
+// para.style.fontFamily = "arial";
+// para.style.color = "red";
+// para.style.backgroundColor = "grey";
+// para.style.fontSize = "30px";
+// para.style.fontFamily = "arial";
+// para.style.fontFamily = "arial";
+// para.style.fontFamily = "arial";
+// para.style.fontFamily = "arial";
+
+// var paras = document.getElementsByTagName("p");
+// console.log(paras);
+
+// paras[0].style.color = "red";
+// paras[1].style.color = "green";
+// paras[2].style.color = "blue";
+// paras[3].style.color = "grey";
+
+// for (var i = 0; i <= paras.length; i++) {
+//   paras[i].style.color = "red";
+// }
