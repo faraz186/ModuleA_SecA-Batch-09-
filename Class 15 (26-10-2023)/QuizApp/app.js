1fc625307c22afafedb896de1b0ea6145608daa4
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-11-25 11:47:57 +0500

    Update app.js

--- Class 15 (26-10-2023)/QuizApp/app.js
+++ Class 15 (26-10-2023)/QuizApp/app.js
@@ -1,60 +1,73 @@
 var questions = [
-{
-    question:"HTML stands for",
-    option1:"Hyper Text markup language",
-    option2:"Hyper Link markup language",
-    option3:"Hyper Text makeup language",
-    correctAns:"Hyper Text markup language"
-},
-{
-    question:"CSS stands for",
-    option1:"Cascading Style sheet",
-    option2:"Cascading Styling sheet",
-    option3:"Cascading super sheet",
-    correctAns:"Cascading Style sheet"
-},{
-    question:"In how many ways can CSS be written in?",
-    option1:"1",
-    option2:"2",
-    option3:"3",
-    correctAns:"3"
-},{
-    question:"Which tag gives your the largest heading in html",
-    option1:"<h6>",
-    option2:"<h2>",
-    option3:"<h1>",
-    correctAns:"<h1>"
-},{
-    question:"how many data types in js?",
-    option1:"6",
-    option2:"7",
-    option3:"8",
-    correctAns:"8"
-}
-,{
-    question:"how many days in febuary",
-    option1:"30",
-    option2:"28",
-    option3:"29",
-    correctAns:"28"
-}]
+{   question: "HTML Stands for",
+    option1: "Hyper Text Markup Language",
+    option2: "Hyper Tech Markup Language",
+    option3: "Hyper Touch Markup Language",
+    corrAnswer: "Hyper Text Markup Language",},
+{   question: "CSS Stands for",
+    option1: "Cascoding Style Sheets",
+    option2: "Cascading Style Sheets",
+    option3: "Cascating Style Sheets",
+    corrAnswer: "Cascading Style Sheets",},
+{   question: "Which tag is used for most large heading",
+    option1: "<h6>",
+    option2: "<h2>",
+    option3: "<h1>",
+    corrAnswer: "<h1>",},
+{   question: "Which tag is used to make element unique ",
+    option1: "id",
+    option2: "class  ",
+    option3: "label",
+    corrAnswer: "id",},
+{   question: "Any element assigned with id, can be get in css ",
+    option1: "by # tag",
+    option2: "by @ tag",
+    option3: "by & tag",
+    corrAnswer: "by # tag",},
+{   question: "CSS can be used with ______ methods ",
+    option1: "8",
+    option2: "3",
+    option3: "4",
+    corrAnswer: "3",},
+{   question: "In JS variable types are ____________ ",
+    option1: "6",
+    option2: "3",
+    option3: "8",
+    corrAnswer: "8",},
+{   question: "In array we can use key name and value ",
+    option1: "True",
+    option2: "False",
+    option3: "None of above",
+    corrAnswer: "False",},
+{   question: "toFixed() is used to define length of decimal ",
+    option1: "True",
+    option2: "False",
+    option3: "None of above",
+    corrAnswer: "True",},
+{   question: "push() method is used to add element in the start of array ",
+    option1: "True",
+    option2: "False",
+    option3: "None of above",
+    corrAnswer: "False", },
+];
 
-
-var para = document.getElementById("ques");
+var question = document.getElementById("ques");
 var opt1 = document.getElementById("opt1");
 var opt2 = document.getElementById("opt2");
 var opt3 = document.getElementById("opt3");
-var button = document.getElementById("btn");
-var timer = document.getElementById("timer")
-var index = 0;
+var button = document.getElementById("btn1");
+var getOptions = document.getElementsByName("options");
+var timer = document.getElementById("timer");
+var quesLength = document.getElementById("ques1")
+var arrLength = questions.length;
+
+var nextQuiz = 0;
 var score = 0;
-var min = 1;
 var sec = 59;
-
-
+var min = 1;
 
 setInterval(function(){
-    timer.innerHTML = `${min}:${sec}`;
+    timer.innerHTML = `Timelaps ${min}:${sec}`;
     sec--
     if(sec<0){
         min--;
@@ -67,52 +80,68 @@ setInterval(function(){
     }
 },1000)
 
+function clicked() {
+    button.disabled = false;
+};
 
-function nextQuestion(){
+function nextQuestion() {
 
-    var getOptions = document.getElementsByName("options");
+        for (var i=0; i<getOptions.length; i++) {
 
-    for(var i = 0;i<getOptions.length;i++)
-    {
-        if(getOptions[i].checked){
-            var selectedValue = getOptions[i].value;
-            // console.log(selectedValue)
-            var selectedQues = questions[index - 1]["question"];
-            var selectedAns = questions[index-1][`option${selectedValue}`]
-            var correctAns = questions[index - 1]["correctAns"]
-            if(selectedAns == correctAns){
-                score++
+            if (getOptions[i].checked) { 
+
+                 var secValue = getOptions[i].value;
+                 var secAnswer = questions[nextQuiz-1][`option${secValue}`];
+                 var corrAnswer = questions[nextQuiz-1].corrAnswer;
+           
+                if (corrAnswer == secAnswer ) {
+                    score++
+                }
             }
+
+            getOptions[i].checked = false;
         }
-        getOptions[i].checked = false
-    }
 
-    button.disabled = true
+    if (nextQuiz > arrLength-1) { 
 
-    if(index >questions.length - 1){
+         button.disabled = true;
+         clearInterval();
 
-        Swal.fire(
-            'Good job!',
-        `Your percentage is ${((score / questions.length)*100).toFixed(2)}`,
-            'success'
-          )
-        // console.log("your percentage is " + ((score / questions.length)*100).toFixed(2))
-    }
-    else{
+        if (score >= 7) {
+            Swal.fire(
+                'Very Good job!',
+            `Your percentage is ${((score / questions.length)*100).toFixed(2)}`,
+                'success')             
+        } else if (score >= 5 && score < 7 ) {
+            Swal.fire(
+                'Good job!',
+            `Your percentage is ${((score / questions.length)*100).toFixed(2)}`,
+                'success') 
+            } else if (score < 5) {
+                Swal.fire(
+                    'Sorry!',
+                `Your percentage is ${((score / questions.length)*100).toFixed(2)}`,
+                    'warning')
+            }       
+        } 
 
-        
-        para.innerHTML = questions[index].question;
-        opt1.innerText = questions[index].option1;
-        opt2.innerText = questions[index].option2;
-        opt3.innerText = questions[index].option3;
-        index++
-    }
-}
+        else {
 
-// nextQuestion()
+ 
+            quesLength.innerHTML = `Question: ${nextQuiz+1} / ${arrLength}`;
 
+            question.innerHTML = questions[nextQuiz].question;
+            opt1.innerText = questions[nextQuiz].option1;
+            opt2.innerText = questions[nextQuiz].option2;
+            opt3.innerText = questions[nextQuiz].option3;
 
-function clicked()
-{
-    button.disabled = false
-}
+            button.disabled = true;
+
+            timer.innerHTML = `2:00`
+
+            min = 1;
+            sec = 59;
+
+            nextQuiz++;
+};
+};
